622dad0391bb4320c3d6737b66e3879a8ba2dee9
Lukas Karle <28999846+lpkarle@users.noreply.github.com>
2025-03-26 21:47:11 +0100

    🧹

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      "Permissions-Policy": "interest-cohort=()",
+    },
+  },
   base: "/ii-V-I/",
 });
