d94aadd42804f665f3d1c5da70fd20ea7a489440
Lukas Karle <28999846+lpkarle@users.noreply.github.com>
2025-04-04 16:33:51 +0200

    Vendor: Add vitest.

---
+++ src/util/sample.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest';
+
+describe('test some module ...', () => {
+  it('should return true', () => {
+    const a = 'a';
+
+    expect(a).toEqual('a');
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/ii-V-I/',
   server: {
     headers: {
       'Permissions-Policy': 'interest-cohort=()',
     },
   },
-  base: '/ii-V-I/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: ['./src/setupTests.ts'],
+  },
 });
